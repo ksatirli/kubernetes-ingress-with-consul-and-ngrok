faa1508833ac0f75009644a2b44a810fbd82e79f
Kerim Satirli <k.satirli@gmail.com>
2023-04-15 23:32:57 +0200

    work-up for cluster configuration

---
+++ configuration/README.md
@@ -0,0 +1,9 @@
+# EKS Cluster Configuration
+
+> This directory contains [AWS](https://registry.terraform.io/providers/hashicorp/aws) Resources.
+
+## Requirements
+
+* [HashiCorp Terraform](https://developer.hashicorp.com/terraform/tutorials/aws-get-started/install-cli) `v1.4.5` or later
+* AWS [account](https://aws.amazon.com/free/)
+
---
+++ configuration/providers.tf
@@ -0,0 +1,12 @@
+# The AWS Provider is set to retrieve configuration from the executing environment
+# see https://registry.terraform.io/providers/hashicorp/aws/latest/docs#schema
+provider "aws" {
+  region = var.aws_region
+}
+
+# see https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs#schema
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.main.token
+}
