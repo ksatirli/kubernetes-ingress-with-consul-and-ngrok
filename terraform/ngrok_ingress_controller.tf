885bfc9f0eced13711f8f80d18fe54e192c167a1
Kerim Satirli <k.satirli@gmail.com>
2023-04-17 15:38:12 +0200

    adds ngrok ingress controller config

---
+++ terraform/ngrok_ingress_controller.tf
@@ -0,0 +1,56 @@
+# see https://github.com/ngrok/kubernetes-ingress-controller/tree/main/docs/examples/consul#installing-the-ingress-controller
+# and https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs/resources/service
+resource "kubernetes_service" "ngrok_ingress_controller" {
+  metadata {
+    name      = "ngrok-ingress-controller-kubernetes-ingress-controller"
+    namespace = "default"
+
+    labels = {
+      app = "ngrok-ingress-controller-kubernetes-ingress-controller"
+    }
+  }
+
+  spec {
+    port {
+      name        = "http"
+      protocol    = "TCP"
+      port        = 80
+      target_port = "80"
+    }
+
+    selector = {
+      "app.kubernetes.io/name" = "kubernetes-ingress-controller"
+    }
+  }
+}
+
+# see https://github.com/ngrok/kubernetes-ingress-controller#installation
+# and https://registry.terraform.io/providers/hashicorp/helm/latest/docs/resources/release
+resource "helm_release" "ngrok" {
+  # default to namespace name, for convenience
+  name = kubernetes_namespace.main.id
+
+  # see https://github.com/ngrok/kubernetes-ingress-controller
+  repository = "https://ngrok.github.io/kubernetes-ingress-controller"
+  chart      = "kubernetes-ingress-controller"
+  namespace  = kubernetes_namespace.main.id
+
+  # see https://github.com/ngrok/kubernetes-ingress-controller/releases/tag/kubernetes-ingress-controller-0.6.0
+  version = "0.6.0"
+
+  values = [
+    templatefile("./templates/ngrok.values.tftpl.yml", {
+      kubernetes_cluster_ip = var.kubernetes_cluster_ip
+    })
+  ]
+
+  set {
+    name  = "credentials.apiKey"
+    value = var.ngrok_apikey
+  }
+
+  set {
+    name  = "credentials.authtoken"
+    value = var.ngrok_authtoken
+  }
+}
