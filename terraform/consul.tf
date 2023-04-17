fa224166feb2e8a919e8c568a566dbe2436f5bd3
Kerim Satirli <k.satirli@gmail.com>
2023-04-17 18:34:51 +0200

    adds consul intention

--- terraform/consul.tf
+++ terraform/consul.tf
@@ -1,5 +1,5 @@
 # see https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs/resources/ingress_v1
-resource "kubernetes_ingress_v1" "ingress_consul" {
+resource "kubernetes_ingress_v1" "consul" {
   metadata {
     name      = "ingress-consul"
     namespace = "default"
@@ -27,6 +27,32 @@ resource "kubernetes_ingress_v1" "ingress_consul" {
           }
         }
       }
+    }
+  }
+}
+
+# see https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs/resources/manifest
+resource "kubernetes_manifest" "consul_intention" {
+  manifest = {
+    "apiVersion" = "consul.hashicorp.com/v1alpha1"
+    "kind"       = "ServiceIntentions"
+
+    "metadata" = {
+      "name"      = "ngrok-ingress-controller-kubernetes-ingress-controller"
+      "namespace" = "default"
+    }
+
+    "spec" = {
+      "destination" = {
+        "name" = "dashboard"
+      }
+
+      "sources" = [
+        {
+          "action" = "allow"
+          "name"   = "ngrok-ingress-controller-kubernetes-ingress-controller"
+        },
+      ]
     }
   }
 }
